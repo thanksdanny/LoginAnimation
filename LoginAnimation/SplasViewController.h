4310480402bfb30ad496926239f34548025bcdc0
thanksdanny <hj-07@163.com>
2016-03-06 20:25:46 +0800

    完成布局

---
+++ LoginAnimation/SplasViewController.h
@@ -0,0 +1,13 @@
+//
+//  SplasViewController.h
+//  LoginAnimation
+//
+//  Created by Danny Ho on 3/6/16.
+//  Copyright © 2016 thanksdanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SplasViewController : UIViewController
+
+@end
